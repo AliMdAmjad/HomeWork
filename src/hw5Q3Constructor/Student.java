4ed915cb2023b8a3bd321c64a747bb4f5ee8672b
AliMdAmjad <amjad.ali.sohel@gmail.com>
2022-05-26 00:06:33 -0400

    update

--- src/hw5Q3Constructor/Student.java
+++ src/hw5Q3Constructor/Student.java
@@ -5,6 +5,7 @@ public class Student {
 	public String stName;
 	public int stID;
 	public char Sex;
+	public float grade;
 	public boolean isProgrammer;
 
 	// default constructor (no argument constructor) declared
@@ -13,12 +14,13 @@ public class Student {
 	}
 
 	// Parameterized constructor declared
-	public Student(String stName, int stId, char Sex, boolean isProgrammer) {
+	public Student(String stName, int stId, char Sex, float grade, boolean isProgrammer) {
 		this.stName = stName;
 		this.stID = stId;
 		this.Sex = Sex;
+		this.grade = grade;
 		this.isProgrammer = isProgrammer;
-		System.out.println("The Student Name is: " + stName + ", Student Id: " + stId + ", Student Sex: " + Sex
+		System.out.println("The Student Name is: " + stName + ", Student Id: " + stId + ", Student Sex: " + Sex + " Grade is :" + grade 
 				+ " and Java Programmer? Ans: " + isProgrammer);
 	}
 
--- src/hw5Q3Constructor/StudentTest.java
+++ src/hw5Q3Constructor/StudentTest.java
@@ -6,10 +6,10 @@ public class StudentTest {
 		Student student = new Student(); // here default Constructor is initialized
 
 		// Parameterized constructor initialized
-		Student mdAli = new Student("Md Ali", 27, 'M', true); // here Constructor is initialized
-		Student mdRahat = new Student("Md Rahat", 12, 'M', true);
-		Student nozhhatTasfia = new Student("Nozhat Tasfia", 33, 'F', true);
-		Student hasinaMomotaz = new Student("Hasina Momotaz", 21, 'F', false);
+		Student mdAli = new Student("Md Ali", 27, 'M', 3.123f, true); // here Constructor is initialized
+		Student mdRahat = new Student("Md Rahat", 12, 'M', 4.123f, true);
+		Student nozhhatTasfia = new Student("Nozhat Tasfia", 33, 'F', 3.345f, true);
+		Student hasinaMomotaz = new Student("Hasina Momotaz", 21, 'F', 4.123f, false);
 
 	}
 
